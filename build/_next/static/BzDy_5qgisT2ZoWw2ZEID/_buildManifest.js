2edbb2db1a9562ef71d87de153f32b22b140d46f
Henry Kanemoto <kanemotohenry@gmail.com>
2024-09-05 21:20:32 -0300

    primeiro commit

---
+++ build/_next/static/BzDy_5qgisT2ZoWw2ZEID/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-81f7c76a672eb059.js"],"/AboutPage":["static/chunks/pages/AboutPage-6581d5567f26a47e.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/AboutPage","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ pages/AboutPage.js
@@ -0,0 +1,7 @@
+export default function AboutScreen() {
+    return( 
+        <div>
+            <h2>Sobre nós</h2>
+        </div>
+    )
+}
