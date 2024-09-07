d3ca2aed26b3337e7bea5e5f2509f812f2675383
Henry Kanemoto <kanemotohenry@gmail.com>
2024-09-07 17:22:02 -0300

    UPDATE novo

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -1,1 +1,11 @@
-console.log("ola mundo");
+import { execSync } from "child_process";
+
+console.log("[DEPLOY_PREVIEW]: START");
+const command = "npm run deploy:staging ";
+const output = execSync(command, { encoding: "utf-8" });
+
+console.log("[DEPLOY_PREVIEW]: START");
+console.log(output);
+
+//console.log("[GITHUB_COMMENT]: START");
+//console.log("[GITHUB_COMMENT]: END");
