3654b2c53b372be85bffb67449b2d828d49ce3c9
Henry Kanemoto <kanemotohenry@gmail.com>
2024-09-07 17:45:21 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function HomeScreen() {
   return (
     <div>
-      <h1>Olá Mundo</h1>
+      <h1>Olá Mundo [NOVA ALTERAÇÃO]</h1>
       <Link href={"./sobre"}>
         <a>Ir para sobre</a>
       </Link>
--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -3,9 +3,11 @@ import { execSync } from "child_process";
 console.log("[DEPLOY_PREVIEW]: START");
 const command = "npm run deploy:staging ";
 const output = execSync(command, { encoding: "utf-8" });
+const outputLines = output.split("\n");
+const DEPLOY_URl = outputLines[outputLines.length - 1];
+console.log("[DEPLOY_PREVIEW]: END");
 
-console.log("[DEPLOY_PREVIEW]: START");
-console.log(output);
+console.log(`You can see the deploly preview on: ${DEPLOY_URl}`);
 
 //console.log("[GITHUB_COMMENT]: START");
 //console.log("[GITHUB_COMMENT]: END");
